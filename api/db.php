457126e05d60b7a9e91e7ea885a90153fffcfc12
Nisal <cmnisal@gmail.com>
2019-05-28 23:53:49 +0530

    servername

--- api/db.php
+++ api/db.php
@@ -6,7 +6,7 @@ Website: https://www.allphptricks.com
 
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("mysqldbuser@admission-lk-site-mysqldbserver","mysqldbuser@admission-lk-site-mysqldbserver","lkadmin@123","admissionlk");
+$con = mysqli_connect("admission-lk-site-mysqldbserver.mysql.database.azure.com","mysqldbuser@admission-lk-site-mysqldbserver","lkadmin@123","admissionlk");
 	if (mysqli_connect_errno()){
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		die();
