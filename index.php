95b234c4400000c5a4061a7aee320d588f6756a5
Nisal <cmnisal@gmail.com>
2019-05-28 23:04:10 +0530

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+include('./api/db.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,7 +33,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="index.html" style="color:white"><img src="img/logo.png"> SRI LANKA EDUCATION MINISTRY <br> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp ADMISSION PORTAL</a>
+      <a class="navbar-brand js-scroll-trigger" href="#" style="color:white"><img src="img/logo.png"> SRI LANKA EDUCATION MINISTRY <br> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp ADMISSION PORTAL</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
