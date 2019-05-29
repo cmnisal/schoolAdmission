88e3081360882bfa67dc1f2c3bf0d2bf1cce83a8
Nisal Chandrasekara <MPL_NisalC@millenniumitesp.com>
2019-05-29 14:39:42 +0530

    bot

--- application-home.php
+++ application-home.php
@@ -4,7 +4,21 @@ include('./api/db.php');
 <!DOCTYPE html>
 
 <html>
+<script type="text/javascript">
+    (function() {
+      var div = document.createElement("div");
+      document.getElementsByTagName('body')[0].appendChild(div);
+      div.outerHTML = "<div id='botDiv' style='height: 38px; position: fixed; bottom: 0; z-index: 1000; background-color: #fff'><div id='botTitleBar' style='height: 38px; width: 400px; position:fixed; cursor: pointer;'></div><iframe width='400px' height='600px' src='https://webchat.botframework.com/embed/admissionlk-bot?s=1OUMRVAAM1Q._ppQD7RYgNLg1XcByl9io9lc46sHOfwvh01U5J_O-Qo'></iframe></div>";
 
+      document.querySelector('body').addEventListener('click', function(e) {
+        e.target.matches = e.target.matches || e.target.msMatchesSelector;
+        if (e.target.matches('#botTitleBar')) {
+          var botDiv = document.querySelector('#botDiv');
+          botDiv.style.height = botDiv.style.height == '600px' ? '38px' : '600px';
+        };
+      });
+    }());
+  </script>
 <head>
     <title>Sri Lanka Education Ministry Admission Portal</title>
     <link rel="shortcut icon" type="image/png" href="/favicon.png" />
--- dashboard.php
+++ dashboard.php
@@ -16,7 +16,21 @@ include('./api/db.php');
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </head>
+<script type="text/javascript">
+    (function() {
+      var div = document.createElement("div");
+      document.getElementsByTagName('body')[0].appendChild(div);
+      div.outerHTML = "<div id='botDiv' style='height: 38px; position: fixed; bottom: 0; z-index: 1000; background-color: #fff'><div id='botTitleBar' style='height: 38px; width: 400px; position:fixed; cursor: pointer;'></div><iframe width='400px' height='600px' src='https://webchat.botframework.com/embed/admissionlk-bot?s=1OUMRVAAM1Q._ppQD7RYgNLg1XcByl9io9lc46sHOfwvh01U5J_O-Qo'></iframe></div>";
 
+      document.querySelector('body').addEventListener('click', function(e) {
+        e.target.matches = e.target.matches || e.target.msMatchesSelector;
+        if (e.target.matches('#botTitleBar')) {
+          var botDiv = document.querySelector('#botDiv');
+          botDiv.style.height = botDiv.style.height == '600px' ? '38px' : '600px';
+        };
+      });
+    }());
+  </script>
 
 
 <body style="background-color:#eee;">
